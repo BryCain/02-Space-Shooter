256bb54f7af6f8610771c8eddeb58aa7c35f2d41
BryCain <brycain@iu.edu>
2022-09-24 14:57:12 -0400

    Update Asteroid_Container.gd

--- Asteroid/Asteroid_Container.gd
+++ Asteroid/Asteroid_Container.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var Asteroid = load("res://Asteroid/Asteroid.tscn")
-var positions = [Vector2(100,100), Vector2(800,100), Vector2(500,500), Vector2(800,500)]
+var positions = [Vector2(100,100), Vector2(800,100), Vector2(500,500), Vector2(800,500), Vector2(1000,1000), Vector2(1200,800), Vector2(800,1200), Vector2(1500,1500)]
 
 func _physics_process(_delta):
 	if get_child_count() < 4:
